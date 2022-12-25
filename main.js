5a62e9f861990290e42017dec16e75acfb0b62bd
mahnoor <m.zamirrsh@gmail.com>
2022-12-25 22:15:33 +0500

    modified

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-// Get the slider element
-const slider = document.querySelector('.slider');
 
-// Add the left button
-const leftButton = document.createElement('button');
-leftButton.innerHTML = '<';
-leftButton.classList.add('slider-button', 'slider-button-left');
-slider.appendChild(leftButton);
 
-// Add the right button
-const rightButton = document.createElement('button');
-rightButton.innerHTML = '>';
-rightButton.classList.add('slider-button', 'slider-button-right');
-slider.appendChild(rightButton);
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
 
-// Get the list of categories
-const categoriesList = document.querySelector('.slider ul');
-
-// Slide the categories to the left when the left button is clicked
-leftButton.addEventListener('click', () => {
-  categoriesList.style.transform = 'translateX(-100%)';
-});
-
-// Slide the categories back to their original position when the right button is clicked
-rightButton.addEventListener('click', () => {
-  categoriesList.style.transform = 'translateX(0)';
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(800);
+  
 });
