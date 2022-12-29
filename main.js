52d27044f57ad9efa5b03311dbb0fe0b559c8ac2
mahnoor <m.zamirrsh@gmail.com>
2022-12-30 03:25:25 +0500

    modified

--- main.js
+++ main.js
@@ -135,26 +135,21 @@ window.onclick = function(e) {
   }
 }
 
-      const sliderMainImage = document.getElementById("product-main-image"); //product container image
-      const sliderImageList = document.getElementsByClassName("image-list"); // image thumblian group selection
-      console.log(sliderImageList);
-      
-      sliderImageList[0].onclick = function(){
-          sliderMainImage.src = sliderImageList[0].src;
-          console.log(sliderMainImage.src);
-      };
-      
-      sliderImageList[1].onclick = function(){
-          sliderMainImage.src = sliderImageList[1].src;
-          console.log(sliderMainImage.src);
-      };
-      
-      sliderImageList[2].onclick = function(){
-          sliderMainImage.src = sliderImageList[2].src;
-          console.log(sliderMainImage.src);
-      };
-      
-      sliderImageList[3].onclick = function(){
-          sliderMainImage.src = sliderImageList[3].src;
-          console.log(sliderMainImage.src);
-      };
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+});
