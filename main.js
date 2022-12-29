8d06990a733f96f8136345178bfad114ed7c64d6
eman <syedaeman999@gmail.com>
2022-12-29 23:48:39 +0500

    Product page added

--- main.js
+++ main.js
@@ -83,4 +83,26 @@ const container = document.querySelector(".container"),
       });
       });
       
->>>>>>> d5bd95728c8a925e0f1efd13d6ae4192115872b8
+      const sliderMainImage = document.getElementById("product-main-image"); //product container image
+      const sliderImageList = document.getElementsByClassName("image-list"); // image thumblian group selection
+      console.log(sliderImageList);
+      
+      sliderImageList[0].onclick = function(){
+          sliderMainImage.src = sliderImageList[0].src;
+          console.log(sliderMainImage.src);
+      };
+      
+      sliderImageList[1].onclick = function(){
+          sliderMainImage.src = sliderImageList[1].src;
+          console.log(sliderMainImage.src);
+      };
+      
+      sliderImageList[2].onclick = function(){
+          sliderMainImage.src = sliderImageList[2].src;
+          console.log(sliderMainImage.src);
+      };
+      
+      sliderImageList[3].onclick = function(){
+          sliderMainImage.src = sliderImageList[3].src;
+          console.log(sliderMainImage.src);
+      };
